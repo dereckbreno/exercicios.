82503a8e3fc2cae2173e8be450b6d137d7f3eb11
dereck breno <dereckfernandes4@gmail.com>
2025-09-16 10:54:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = "dereck";   // sua variável com o nome
+console.log(nome)
+let idade = 22;     // sua variável com a idade
+console.log(idade)
+alert("Olá, meu nome é " + nome + " e tenho " + idade + " anos!");
